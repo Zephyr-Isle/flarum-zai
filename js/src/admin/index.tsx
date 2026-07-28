819d0360c97d199e8750b6f74d6c5fefcd99072f
Zephyr-Isle <zephyr_isle@outlook.com>
2026-07-28 22:05:22 +0800

    fix: add initializer to admin index.tsx

--- js/src/admin/index.tsx
+++ js/src/admin/index.tsx
@@ -1,1 +1,7 @@
 export { default as extend } from './extend';
+
+import app from 'flarum/admin/app';
+
+app.initializers.add('zephyrisle-flarum-zai-bot', () => {
+  // Imperative logic (if any) goes here
+});
